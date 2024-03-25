74506f810894773db5351a0515fb3a390c68076b
ltaylor2 <ltaylor.ulysses@gmail.com>
2024-03-25 00:03:00 -0400

    map working

--- CTBirds_Dashboard/app.r
+++ CTBirds_Dashboard/app.r
@@ -1,49 +1,98 @@
 library(tidyverse)
+library(sf)
 library(shiny)
 
 # Runtime ---------------------------
-taxonomy <- read_csv("taxonomy.csv", show_col_types=FALSE)
+taxonomy <- readRDS("taxonomy.RDS")
 taxonomyDict <- taxonomy$Taxon_Scientific
 names(taxonomyDict) <- taxonomy$Taxon_Common
-obs <- read_csv("ebd_ct_cleaned_observation_proportions.csv", show_col_types=FALSE)
+proportions <- readRDS("species_observations_proportions.RDS")
+locations <- readRDS("species_observations_locations.RDS")
+maxWeeks <- readRDS("species_observations_maxWeeks.RDS")
+changeModels <- readRDS("species_observations_changeModels.RDS")
+ctMap <- readRDS("ct_shapefile.RDS")
 
 # ui ---------------------------
 ui <- fluidPage(
-   fluidRow(column(width=11, offset=0, plotOutput("plot"))),
+   titlePanel(h1("CT Birds Dashboard", align="center"), windowTitle="CT Birds Dashboard"),
+   h3(textOutput("titleCommon"), em(textOutput("titleScientific")), align="center"), 
+   fluidRow(column(width=6, offset=0, plotOutput("timeseries")), column(width=6, offset=0, plotOutput("map"))),
    fluidRow(column(width=3, offset=1, selectizeInput("common", label="Species", choices=names(taxonomyDict), selected="Tree Swallow"))),
-   fluidRow(column(width=10, offset=1, helpText("For each week of 2024, boxplots summarize the percentage of eBird checklists in which the selected species was observed during previous years. The black line shows the general trend by smoothing across all points."))),
-   fluidRow(column(width=10, offset=1, helpText("For example: On the 13th week of 2015, 55 of 860 checklists (6.4%) featured Tree Swallows. On the 13th week of 2020, 121 of 1161 checklists (10.4%) featured Tree Swallows. The 13th week of 2024 begins on March 25."))),
-   fluidRow(column(width=10, offset=1, helpText("Upper and lower hinges of each box indicate 25th and 75th percentiles, summarizing across 9 years of eBird data for each week (2015-2023). Whiskers for each box extend to the smallest/largest values within 1.5x the height of the box. Points show outliers. Trend line smoothed with LOESS (span = 0.1)."))),
-   fluidRow(column(width=10, offset=1, helpText("Data from 548,593 complete eBird checklists in Connecticut.")))
+   fluidRow(column(width=9, offset=1, helpText(strong("Data from 548,593 complete eBird checklists in Connecticut, 2015-2023 only.")))),
+   fluidRow(column(width=9, offset=1, helpText(strong("Left (time series):"), 
+                                              p("For each week of 2024, boxplots summarize the percentage of eBird checklists in which the selected species was observed during previous years."),
+                                              p("Black line shows the general trend by smoothing across all points."),
+                                              p("Upper and lower hinges of each box indicate 25th and 75th percentiles. Whiskers extend to the smallest/largest values within 1.5x the height of the box. Points show outliers. Trend line smoothed with LOESS (span = 0.1)."),
+                                              p("For example: On the 13th week of 2015, 55 of 860 checklists (6.4%) featured Tree Swallows. On the 13th week of 2020, 121 of 1161 checklists (10.4%) featured Tree Swallows. The 13th week of 2024 begins on March 25.")))),
+   fluidRow(column(width=9, offset=1, helpText(strong("Right (map):"), 
+                                               p("Points show the location of checklists featuring the species."),
+                                               p("Brighter colors highlight regions where the greatest numbers of individual birds for that species have been observed in any one checklist."),
+                                               p("For example: a bright red hexagon could indicate ~1000 Tree Swallows seen in one checklist in the area, whereas a faded hexagon indicates a maximum record of only 10 Tree Swallows.
+                                                  Colors are relative for each species; red for Tree Swallows could indicate hundreds of birds, whereas red for Kentucky Warblers could indicate a single bird."))))
 )
 
 # server ---------------------------
 server <- function(input, output, session) {
-    subsetted <- reactive({
+
+    output$titleCommon <- renderText({
+      input$common
+    })
+    
+    output$titleScientific <- renderText({
+      if (input$common=="") {
+        ""
+      } else {
+        taxonomyDict[[input$common]]
+      }
+    })
+    
+    output$modeledChange <- renderText({
+      paste0("CT population: ", changeModels[changeModels$Taxon_Scientific==taxonomyDict[[input$common]],"Change"],
+            " (",changeModels[changeModels$Taxon_Scientific==taxonomyDict[[input$common]],"Significance"],")")
+    })
+    
+    subsetProportions <- reactive({
         req(input$common)
-        filter(obs, Taxon_Scientific==taxonomyDict[[input$common]])
+        filter(proportions, Taxon_Scientific==taxonomyDict[[input$common]])
     })
 
-    output$plot <- renderPlot({
-        df <- subsetted()
+    subsetLocations <- reactive({
+        req(input$common)
+        filter(locations, Taxon_Scientific==taxonomyDict[[input$common]])
+    })
+
+    output$timeseries <- renderPlot({
+        df <- subsetProportions()
         taxon_scientific <- df$Taxon_Scientific[1]
         taxon_common <- names(taxonomyDict)[taxonomyDict==taxon_scientific]
         
         ggplot(df, aes(x=Week_of_2024, y=Percentage_Checklists_Observed)) +
-         geom_boxplot(aes(group=Week_of_2024), colour="darkgray", fill="transparent", alpha=0.3, outlier.size=0.5) +
-         geom_smooth(formula="y~x", method="loess", span=0.15, colour="black", se=FALSE, linewidth=0.8) +
-         scale_x_date(date_breaks="1 month",
-                      date_labels="%b") +
-         scale_y_continuous(limits=c(0, 100), breaks=seq(0, 100, by=10),
+            geom_vline(xintercept=maxWeeks[taxon_scientific], colour="red", alpha=0.15, linewidth=2) +
+            geom_boxplot(aes(group=Week_of_2024), colour="darkgray", fill="transparent", alpha=0.3, outlier.size=0.5) +
+            geom_smooth(formula="y~x", method="loess", span=0.15, colour="black", se=FALSE, linewidth=0.8) +
+            scale_x_date(date_breaks="1 month",
+                        date_labels="%b") +
+            scale_y_continuous(limits=c(0, 100), breaks=seq(0, 100, by=10),
                             labels=~paste0(.,"%")) +
-         xlab("Predicted 2024") +
-         ylab("CT checklists with species observed (2015-2023)") +
-         ggtitle(bquote(.(taxon_common)~"("*italic(.(taxon_scientific))*")")) +
-         theme_bw() +
-         theme(axis.text.x=element_text(size=10),
-               panel.grid.major=element_line(color=alpha("#f0f0f0", 0.5)),
-               panel.grid.minor.x=element_blank(),
-               panel.grid.minor.y=element_blank())
-    }, res=100) 
+            xlab("Predicted 2024") +
+            ylab("Species observed (2015-2023)") +
+            theme_bw() +
+            theme(axis.text.x=element_text(size=10),
+                  panel.grid.major=element_line(color=alpha("#f0f0f0", 0.5)),
+                  panel.grid.minor.x=element_blank(),
+                  panel.grid.minor.y=element_blank())
+    }, res=96) 
+
+    output$map <- renderPlot({
+        df <- subsetLocations()
+
+        ggplot(df) +
+            geom_hex(aes(x=Longitude, y=Latitude, weight=Log10_Max_Observed), alpha=0.80, colour="transparent", binwidth=c(0.08, 0.06)) +
+            geom_point(aes(x=Longitude, y=Latitude), size=0.1, colour="black") +
+            geom_sf(data=ctMap, colour="black", fill="transparent", linewidth=1.3) +
+            scale_fill_gradient(low="transparent", high="red") +
+            guides(fill="none") +
+            theme_void()
+    }, res=96) 
 }
 shinyApp(ui, server)
--- prepare_datasets.r
+++ prepare_datasets.r
@@ -1,12 +1,14 @@
 library(tidyverse)
-library(auk)
-library(tigris)
+# library(auk)
+# library(tigris)
 
-taxonomy <- get_ebird_taxonomy()
-ctSpecies <- tibble(Taxon_Common = readLines("ct_species_list.txt")) |>
-          left_join(taxonomy, by=c("Taxon_Common"="common_name")) |>
-          select(Taxon_Common, Taxon_Scientific=scientific_name, Family=family, Order=order)
-write_csv(ctSpecies, "taxonomy.csv")
+# # taxonomy <- get_ebird_taxonomy()
+# ctSpecies <- tibble(Taxon_Common = readLines("ct_species_list.txt")) |>
+#           left_join(taxonomy, by=c("Taxon_Common"="common_name")) |>
+#           select(Taxon_Common, Taxon_Scientific=scientific_name, Family=family, Order=order)
+# write_csv(ctSpecies, "taxonomy.csv")
+
+# saveRDS(ctSpecies, file="taxonomy.RDS")
 
 # auk_ebd("ebd_US-CT_smp_relJan-2024.txt",
 #         file_sampling="ebd_US-ct_smp_relJan-2024_sampling.txt") |>
@@ -27,53 +29,120 @@ write_csv(ctSpecies, "taxonomy.csv")
 # observations <- read_csv("zerofilled_observations.csv", lazy=FALSE) |>
 #              select(ID = checklist_id, Taxon_Scientific = scientific_name, Observed = species_observed)
 
-sampling <- read_csv("zerofilled_sampling.csv", lazy=TRUE) |>
-          select(ID = checklist_id, Date = observation_date, Latitude=latitude, Longitude=longitude) |>
-          mutate(Week=week(Date), Year=year(Date)) 
+# sampling <- read_csv("zerofilled_sampling.csv", lazy=TRUE) |>
+#           select(ID = checklist_id, Date = observation_date, Latitude=latitude, Longitude=longitude) |>
+#           mutate(Week=week(Date), Year=year(Date)) 
 
-summarizeSpeciesObs <- function(taxon) {
-    cat(paste0("Summarizing observations for ", taxon, ".\n"))
+# summarizeSpeciesObs <- function(taxon) {
+#     cat(paste0("Summarizing observations for ", taxon, ".\n"))
 
-    obs <- read_csv(paste0("Zerofilled_By_Species/", str_replace(taxon, " ", "_"), ".csv"), show_col_types=FALSE, col_types="cccccccl") |>
-        select(ID = checklist_id, Taxon_Scientific = scientific_name, Observed = species_observed) |>
-        mutate(Observed = ifelse(Observed, "Yes", "No")) |>
-        left_join(sampling, by="ID")
+#     obs <- read_csv(paste0("Zerofilled_By_Species/", str_replace(taxon, " ", "_"), ".csv"), show_col_types=FALSE, col_types="cccccccl") |>
+#         mutate(Observation_Count = as.numeric(ifelse(observation_count=="X", 1, observation_count))) |>
+#         select(ID = checklist_id, Taxon_Scientific = scientific_name, Observed = species_observed, Observation_Count) |>
+#         mutate(Observed = ifelse(Observed, "Yes", "No")) |>
+#         left_join(sampling, by="ID")
         
-    proportions <- obs |>
-                mutate(Week = week(Date), Year=year(Date)) |>
-                group_by(Year, Week, Observed) |>
-                tally(name="N") |>
-                pivot_wider(id_cols=c(Year, Week), names_from=Observed, values_from=N, values_fill=0) |>
-                mutate(Percentage_Checklists_Observed = Yes / (Yes+No) * 100,
-                       Week_of_2024 = ymd("2024-01-01") + weeks(Week-1),
-                       Taxon_Scientific = taxon) |>
-                ungroup() |>
-                select(Taxon_Scientific, Year, Week_of_2024, Percentage_Checklists_Observed)
+#     proportions <- obs |>
+#                 mutate(Week = week(Date), Year=year(Date)) |>
+#                 group_by(Year, Week, Observed) |>
+#                 tally(name="N") |>
+#                 pivot_wider(id_cols=c(Year, Week), names_from=Observed, values_from=N, values_fill=0) |>
+#                 mutate(Percentage_Checklists_Observed = Yes / (Yes+No) * 100,
+#                        Week_of_2024 = ymd("2024-01-01") + weeks(Week-1),
+#                        Taxon_Scientific = taxon) |>
+#                 ungroup() |>
+#                 select(Taxon_Scientific, Year, Week_of_2024, Percentage_Checklists_Observed)
     
-    locations <- obs |>
-              filter(Observed == "Yes") |>
-              select(Taxon_Scientific, Date, Latitude, Longitude)
+#     locations <- obs |>
+#               filter(Observed == "Yes") |>
+#               group_by(Taxon_Scientific, Latitude, Longitude) |>
+#               summarize(Max_Observed = max(Observation_Count), .groups="drop") |>
+#               mutate(Log10_Max_Observed = log10(Max_Observed)) |>
+#               select(Taxon_Scientific, Latitude, Longitude, Log10_Max_Observed)
 
-    return(list("Proportions"=proportions, "Locations"=locations))
-}
+#     return(list("Proportions"=proportions, "Locations"=locations))
+# }
 
-cleaned <- map(ctSpecies$Taxon_Scientific, summarizeSpeciesObs)
+# cleaned <- map(ctSpecies$Taxon_Scientific, summarizeSpeciesObs)
 
-proportions <- map_df(1:length(cleaned), ~cleaned[[.]]$Proportions)
-saveRDS(proportions, file="species_observations_proportions.RDS")
+# proportions <- map_df(1:length(cleaned), ~cleaned[[.]]$Proportions)
+# saveRDS(proportions, file="species_observations_proportions.RDS")
 
-locations <- map_df(1:length(cleaned), ~cleaned[[.]]$Locations)
-saveRDS(locations, file="species_observations_locations.RDS")
+# locations <- map_df(1:length(cleaned), ~cleaned[[.]]$Locations)
+# saveRDS(locations, file="species_observations_locations.RDS")
 
 # CT shapefiles
-states <- states(cb = TRUE, resolution = "500k")
+# states <- states(cb = TRUE, resolution = "500k")
 
-ct <- subset(states, NAME=="Connecticut")
+# ct <- subset(states, NAME=="Connecticut")
+# saveRDS(ct, file="ct_shapefile.RDS")
 
-temp <- locations |>
-     filter(Taxon_Scientific=="Branta canadensis")
-ggplot(temp) +
-    geom_sf(data=ct, colour="black", fill="white") +
-    geom_point(aes(x=Longitude, y=Latitude)) +
-    theme_void()
+# maxWeek <- proportions |>
+#         group_by(Taxon_Scientific, Week_of_2024) |>
+#         summarize(Percentage_Checklists_Observed = median(Percentage_Checklists_Observed)) |>
+#         slice_max(order_by=Percentage_Checklists_Observed, with_ties=FALSE)
+# maxes <- maxWeek$Week_of_2024
+# names(maxes) <- maxWeek$Taxon_Scientific         
+# saveRDS(maxes, file="species_observations_maxWeeks.RDS")
 
+# summarizeChanges <- function(taxon) {
+#     df <- filter(proportions, Taxon_Scientific==taxon)
+#     fit <- lm(Percentage_Checklists_Observed ~ Year + Week_of_2024, data=df) |>
+#         summary()
+#     estimate <- fit$coefficients["Year", "Estimate"]
+#     estimateSummary <- "No Change"
+#     if (estimate < 0) { 
+#         estimateSummary <- "Decreasing" 
+#     } else if (estimate > 0) {
+#         estimateSummary <- "Increasing"
+#     }
+    
+#     p <- fit$coefficients["Year", 4] 
+#     pSummary <- "N.S."
+#     if (p < 0.05) { 
+#         pSummary <- "P < 0.05"
+#     }
+#     if (p < (0.05 / nrow(taxonomy))) {
+#         pSummary <- "True significance"
+#     }
+#     ret <- tibble(Taxon_Scientific=taxon, Change=estimateSummary, Significance = pSummary)
+#     return(ret)
+# }
+# changes <- map_df(taxonomy$Taxon_Scientific, summarizeChanges)
+# saveRDS(changes, file="species_observations_changeModels.RDS")
+
+# proportions <- readRDS("species_observations_proportions.RDS")
+# taxonomy <- readRDS("taxonomy.RDS")
+
+# summarizeSeasonalPattern <- function(taxon) {
+#     df <- filter(proportions, Taxon_Scientific==taxon) |>
+#     mutate(Week = week(Week_of_2024)) |>
+#     mutate(Week2 = Week^2,
+#             Week3 = Week^3,
+#             Week4 = Week^4)
+#     uniformFit <- lm(Percentage_Checklists_Observed ~ 1, data=df)
+#     quadraticFit <- lm(Percentage_Checklists_Observed ~ Week + Week2, data=df)
+#     quarticFit <- lm(Percentage_Checklists_Observed ~ Week + Week2 + Week3 + Week4, data=df)
+#     aics <- map_dbl(list(uniformFit, quadraticFit, quarticFit), AIC)
+
+#     # Default: uniform model preferred
+#     seasonality <- "Uniform (common)"
+#     if (uniformFit$coefficients[[1]] < 2.5) {
+#         seasonality <- "Uniform (rare)"
+#     }
+#     if (aics[2] < (aics[1] - 5)) {
+#         # Quadratic fit preferred
+#         seasonality <- "Summer"
+#         if (quadraticFit$coefficients["Week2"]>0) {
+#             seasonality <- "Winter"
+#         }
+#     }
+#     if (aics[3] < (aics[2] - 5) & aics[3] < (aics[1] - 5)) {
+#         # Migratory fit preferred
+#         seasonality <- "Migratory"
+#     }
+#     return(tibble(Taxon_Scientific=taxon, Seasonality=seasonality))
+# }
+# seasonalities <- map_df(taxonomy$Taxon_Scientific, summarizeSeasonalPattern)
+
+# seasonalities |> left_join(taxonomy) |> View()
